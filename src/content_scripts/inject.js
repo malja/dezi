7297e553d850a05cd4d95424cdbf8c877aadc404
Jan Malčák <github@malcak.cz>
2019-09-24 16:46:55 +0200

    New modal popup
    Solved #3

--- src/content_scripts/inject.js
+++ src/content_scripts/inject.js
@@ -1,33 +1,85 @@
-/**
- * Show popup to inform the user he/she is visiting dangerous site.
- * TODO: Edit popup overlay.
+/*
+ * Copyright (c) 2019 Jan Malčák
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
-const showPopup = () => {
 
-	fetch(chrome.extension.getURL('./inject.html'))
+/**
+ * Show warning modal window to inform user they are visiting dangerous site.
+ */
+function showPopup(url) {
+	// Stop page loading - make sure they are not blocking us
+	window.stop();
+
+	// Load modal's html code from given file
+	fetch(chrome.extension.getURL('./src/content_scripts/modal.html'))
     	.then(response => response.text())
     	.then(data => {
-        	document.body.innerHTML += data;
-    	}).catch(err => {
-        	// TODO: handle error
-    	});
+
+			// Append my modal to <html> tag
+			document.documentElement.innerHTML += data;
+
+			// Attach event listeners to <Continue> ...
+			document.getElementById("dezi-modal-close").onclick = () => {
+				window.location.reload();
+				chrome.runtime.sendMessage({
+					target: "bg", 
+					type: "updateWebsiteSettings", 
+					data: {
+						url: window.location.href,
+						ignore: false 
+					}
+				});
+			}
+
+			// ... and <Continue and Ignore> buttons
+			document.getElementById("dezi-modal-ignore").onclick = () => {
+				window.location.reload();
+				chrome.runtime.sendMessage({
+					target: "bg",
+					type: "updateWebsiteSettings", 
+					data: {
+						url: window.location.href,
+						ignore: true 
+					}
+				});
+			}
+		});
 }
 
 /**
- * This function checks if the site is on the blacklist.
+ * Request info about current tab's URL from background page and show modal
+ * window when necessary.
  */
-chrome.runtime.sendMessage({target: "bg", type: "checkURL", data: window.location}, (response) => {
+chrome.runtime.sendMessage({target: "bg", type: "checkURL", data: window.location.href}, (response) => {
 	let website = response.data;
-	if (website.isDangerous) {
-		document.getElementsByTagName("body")[0].classList.add("dangerous-site");
 
-		let now = Date.now().getTime();
-		let last_popup = Date.parse(website.userSettings.lastPopup).getTime();
+	if (website.isDangerous) {
+
+		// Calculate number of seconds since last popup was shown for current URL
+		let now = new Date().getTime();
+		let last_popup = new Date(website.userSettings.lastPopup).getTime();
 		let seconds_since_last_popup = (now - last_popup)/1000;
 
 		// TODO: Change to dynamicaly loaded number of seconds
 		if (seconds_since_last_popup > 30 && !website.userSettings.ignore) {
-			showPopup();
+			showPopup(website.url);
 		}
 	}
 });
